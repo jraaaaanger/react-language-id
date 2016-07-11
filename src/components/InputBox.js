2e4dc182ae6eb081e5f1cd981af08cb7d6d3d964
Jimmy Ranger <jranger@me.com>
2016-07-10 21:44:15 -0400

    got it working

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,5 +1,49 @@
 import React from 'react';
 
+var languageCodes = {
+  'af': 'Afrikaans',
+  'am': 'Amharic',
+  'bg': 'Bulgarian',
+  'ca': 'Catalan',
+  'zh': 'Chinese',
+  'hr': 'Croatian',
+  'cs': 'Czech',
+  'da': 'Danish',
+  'nl': 'Dutch',
+  'en': 'English',
+  'et': 'Estonian',
+  'fil': 'Filipino',
+  'fi': 'Finnish',
+  'fr': 'French',
+  'de': 'German',
+  'el': 'Greek',
+  'hi': 'Hindi',
+  'hu': 'Hungarian',
+  'id': 'Indonesian',
+  'in': 'Indonesian',
+  'it': 'Italian',
+  'ja': 'Japanese',
+  'ko': 'Korean',
+  'lv': 'Latvian',
+  'lt': 'Lithuanian',
+  'ms': 'Malay',
+  'no': 'Norwegian',
+  'pl': 'Polish',
+  'pt': 'Portuguese',
+  'ro': 'Romanian',
+  'ru': 'Russian',
+  'sr': 'Serbian',
+  'sk': 'Slovak',
+  'sl': 'Slovenian',
+  'es': 'Spanish',
+  'sw': 'Swahili',
+  'sv': 'Swedish',
+  'th': 'Thai',
+  'tr': 'Turkish',
+  'uk': 'Ukranian',
+  'zu': 'Zulu'
+}
+
 class InputBox extends React.Component {
   constructor(props) {
     super(props);
@@ -12,7 +56,7 @@ class InputBox extends React.Component {
 
   componentWillUpdate(nextProps, nextState) {
     let sourceText = encodeURI(this.state.inputText);
-    var source = 'https://www.googleapis.com/language/translate/v2/detect?key=KEY&q=' + sourceText;
+    var source = 'https://www.googleapis.com/language/translate/v2/detect?key=AIzaSyACJqo1v66r9BgpWZxHMoaRrUoFGYJABik&q=' + sourceText;
     var that = this;
       $.ajax ({
         dataType: 'json',
@@ -27,17 +71,24 @@ class InputBox extends React.Component {
   onChange() {
     let updatedText = $('.text-box')[0].value;
     if (updatedText == "") {
-      updatedText = null;
+      this.setState({ inputText: null })
     } else {
-    this.setState({ inputText: updatedText });
+      this.setState({ inputText: updatedText });
     }
   }
 
   render() {
+    var lID = this.state.languageID;
+    var language = languageCodes[lID];
+    var sentence = "";
+
+    if (language) {
+      sentence = "This is " + language;
+    };
     return (
       <div>
         <input className="text-box" type="text" onChange={this.onChange}></input>
-        <h4>{this.state.languageID}</h4>
+        <h4>{sentence}</h4>
       </div>
     );
   }
